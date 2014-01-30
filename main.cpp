d2f67ed60265611be068f6a862470ae807a4605c
Xavier <sseingalt@gmail.com>
2014-01-30 21:23:17 +0100

    add some more strings

--- main.cpp
+++ main.cpp
@@ -11,15 +11,19 @@ int main()
 
     OneRandom.List.push_back(std::make_pair("1_test", 1));
     OneRandom.List.push_back(std::make_pair("100_test", 2));
+    OneRandom.List.push_back(std::make_pair("351_test", 3));
+    OneRandom.List.push_back(std::make_pair("353_test", 3));
     OneRandom.List.push_back(std::make_pair("350_test", 3));
+    OneRandom.List.push_back(std::make_pair("354_test", 3));
+    OneRandom.List.push_back(std::make_pair("352_test", 3));
+    OneRandom.List.push_back(std::make_pair("353_test", 3));
 
     OneRandom.display();
 
     std::sort(OneRandom.List.begin(), OneRandom.List.end(), OneRandom.MyComp);
 
     OneRandom.display();
-
-    Class::List_t::iterator it = OneRandom.binarySearch(OneRandom.List.begin(),OneRandom.List.end(),"two");
+    Class::List_t::iterator it = OneRandom.binarySearch(OneRandom.List.begin(),OneRandom.List.end(),"351_test");
 
     std::cout << "result \n";
 
